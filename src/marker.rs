c0a2ea65ee3a2e235219d59c468951949c2ce874
ubmu <uubmu@proton.me>
2026-03-30 00:32:53 -0400

    `Marker` enum with FourCC & UUID support

---
+++ src/marker.rs
@@ -0,0 +1,46 @@
+use crate::{Error, Result};
+use std::fmt;
+
+/// A block identifier marker.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Marker {
+    /// Four-character code (FourCC).
+    FourCC([u8; 4]),
+    /// UUID or GUID.
+    UUID([u8; 16]),
+}
+
+impl TryFrom<[u8; 4]> for Marker {
+    type Error = Error;
+    fn try_from(bytes: [u8; 4]) -> Result<Self> {
+        Ok(Marker::FourCC(bytes))
+    }
+}
+
+// impl TryFrom<[u8; 16]> for Marker {}
+
+impl fmt::Display for Marker {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Marker::FourCC(bytes) => match std::str::from_utf8(bytes) {
+                Ok(s) => write!(f, "{}", s),
+                Err(_) => write!(f, "{:08X}", u32::from_le_bytes(*bytes)),
+            },
+            Marker::UUID(bytes) => {
+                write!(
+                    f,
+                    "{:08X}-{:04X}-{:04X}-{:04X}-{:012X}",
+                    u32::from_le_bytes(bytes[0..4].try_into().unwrap()),
+                    u16::from_le_bytes(bytes[4..6].try_into().unwrap()),
+                    u16::from_le_bytes(bytes[6..8].try_into().unwrap()),
+                    u16::from_be_bytes(bytes[8..10].try_into().unwrap()),
+                    {
+                        let mut buf = [0u8; 8];
+                        buf[2..].copy_from_slice(&bytes[10..16]);
+                        u64::from_be_bytes(buf)
+                    }
+                )
+            }
+        }
+    }
+}
