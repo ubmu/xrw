9644916b344bc8126824cb9c84e1de3629072171
ubmu <uubmu@proton.me>
2026-04-01 17:32:48 -0400

    Add basic functionality for `Kind`

--- src/kind.rs
+++ src/kind.rs
@@ -1,5 +1,16 @@
+use crate::{Error, Marker, Result};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Kind {
     WAVE,
-    AIFF,
+}
+
+impl TryFrom<Marker> for Kind {
+    type Error = Error;
+    fn try_from(marker: Marker) -> Result<Self> {
+        match marker {
+            Marker::WAVE => Ok(Kind::WAVE),
+            _ => Err(Error::UnknownKind),
+        }
+    }
 }
