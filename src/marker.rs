0242dc5578577fd57825b864b46bd4825d88d486
ubmu <uubmu@proton.me>
2026-04-01 17:34:41 -0400

    Create `minimum_payload_size` and some marker constants

--- src/marker.rs
+++ src/marker.rs
@@ -10,6 +10,38 @@ pub enum Marker {
     UUID([u8; 16]),
 }
 
+impl Marker {
+    //
+    pub const RIFF: Self = Self::FourCC(*b"RIFF");
+    pub const RIFX: Self = Self::FourCC(*b"RIFX");
+    pub const FFIR: Self = Self::FourCC(*b"FFIR");
+    pub const RF64: Self = Self::FourCC(*b"RF64");
+    pub const BW64: Self = Self::FourCC(*b"BW64");
+    /// Sony Wave64 stores the FourCC in the first four bytes of its UUID identifier,
+    /// allowing detection without reading the full UUID.
+    pub const SW64: Self = Self::FourCC(*b"riff");
+
+    // TODO: Figure out a general doc line for these.
+
+    pub const WAVE: Self = Self::FourCC(*b"WAVE");
+    // Markers for RIFF chunks.
+    pub const DS64: Self = Self::FourCC(*b"ds64");
+    pub const FMT: Self = Self::FourCC(*b"fmt ");
+    pub const FMT_: Self = Self::FourCC(*b"fmt ");
+    pub const DATA: Self = Self::FourCC(*b"data");
+    pub const BEXT: Self = Self::FourCC(*b"bext");
+    pub const LIST: Self = Self::FourCC(*b"LIST");
+
+    pub fn minimum_payload_size(&self) -> u64 {
+        match self {
+            &Self::FMT => 16,
+            // TODO: See if there is a better default value.
+            // Set to 1 as we check if payload size is less than minimum size.
+            _ => 1,
+        }
+    }
+}
+
 impl TryFrom<[u8; 4]> for Marker {
     type Error = Error;
     fn try_from(bytes: [u8; 4]) -> Result<Self> {
