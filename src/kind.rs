989a87eaea2bdb4ed5b54b3d00d712cc2fd6e99b
ubmu <uubmu@proton.me>
2026-04-01 23:49:47 -0400

    Add AIFF/AIFC `Kind` support

--- src/kind.rs
+++ src/kind.rs
@@ -3,6 +3,8 @@ use crate::{Error, Marker, Result};
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Kind {
     WAVE,
+    AIFF,
+    AIFC,
 }
 
 impl TryFrom<Marker> for Kind {
@@ -10,6 +12,8 @@ impl TryFrom<Marker> for Kind {
     fn try_from(marker: Marker) -> Result<Self> {
         match marker {
             Marker::WAVE => Ok(Kind::WAVE),
+            Marker::AIFF => Ok(Kind::AIFF),
+            Marker::AIFC => Ok(Kind::AIFC),
             _ => Err(Error::UnknownKind),
         }
     }
