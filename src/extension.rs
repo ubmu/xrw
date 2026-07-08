d9ef636509f9826ccd2429cad42de7a7806077ab
ubmu <uubmu@proton.me>
2026-07-08 00:25:07 -0400

    Include `Extension::CoreAudioHeader`.

--- src/extension.rs
+++ src/extension.rs
@@ -3,6 +3,8 @@
 pub enum Extension {
     /// 64-bit size extension for RF64 and BW64 containers.
     Ds64(Ds64),
+    /// The file version and flags for CAF.
+    CoreAudioHeader(CoreAudioHeader),
 }
 
 /// The `ds64` chunk, required in RF64 and BW64 files.
@@ -21,4 +23,15 @@ pub struct Ds64 {
     pub data_size: u64,
     /// True sample count, replacing the value in the `fact` chunk.
     pub sample_count: u64,
+}
+
+/// The header for CAF.
+///
+/// Apple Core Audio Format Specification 1.0: https://developer.apple.com/library/archive/documentation/MusicAudio/Reference/CAFSpec/CAF_spec/CAF_spec.html
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CoreAudioHeader {
+    // The file version. For CAF files conforming to this specification, the version must be set to 1.
+    pub file_version: u16,
+    // Flags reserved by Apple for future use. For CAF v1 files, must be set to 0.
+    pub file_flags: u16,
 }
