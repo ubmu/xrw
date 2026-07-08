88df379831597311db26a40e30c77b6491f4077e
ubmu <uubmu@proton.me>
2026-07-08 01:34:03 -0400

    Include CAF detection and fix enum naming.

--- src/container.rs
+++ src/container.rs
@@ -9,16 +9,14 @@ use crate::{Error, Result};
 /// The container format of a structured binary file.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Container {
-    /// Interchange File Format
-    IFF,
-    /// Resource Interchange File Format
-    RIFF,
-    /// RIFF, any big-endian variant
-    RIFX,
-    /// RIFF 64-bit
-    RF64,
-    /// Sony Wave64
-    SW64,
+    Interchange,
+    ResourceInterchange,
+    /// RIFF, any big-endian variant.
+    ResourceInterchangeBE,
+    /// 64-bit RIFF.
+    ResourceInterchange64,
+    SonyWave64,
+    CoreAudio,
 }
 
 impl Container {
@@ -29,7 +27,9 @@ impl Container {
             return Ok(container);
         }
 
-        // Add other try_detect_ functions as they come.
+        if let Ok(container) = Self::try_detect_coreaudio(reader) {
+            return Ok(container);
+        }
 
         Err(Error::UnknownContainer)
     }
@@ -40,14 +40,24 @@ impl Container {
         let marker = Marker::from(reader.read_property_code()?);
         Self::try_from(marker)
     }
+
+    /// Attempts to identify Core Audio Format.
+    fn try_detect_coreaudio<R: Read + Seek>(reader: &mut Reader<R>) -> Result<Self> {
+        reader.seek(0)?;
+        let marker = Marker::from(reader.read_property_code()?);
+        Self::try_from(marker)
+    }
 }
 
 impl From<&Container> for Descriptor {
     fn from(container: &Container) -> Self {
         match container {
-            Container::IFF | Container::RIFX => Descriptor::IFF,
-            Container::RIFF | Container::RF64 => Descriptor::RIFF,
-            Container::SW64 => Descriptor::SW64,
+            Container::Interchange | Container::ResourceInterchangeBE => Descriptor::INTERCHANGE,
+            Container::ResourceInterchange | Container::ResourceInterchange64 => {
+                Descriptor::RESOURCE_INTERCHANGE
+            }
+            Container::SonyWave64 => Descriptor::SONY_WAVE64,
+            Container::CoreAudio => Descriptor::CORE_AUDIO,
         }
     }
 }
@@ -57,11 +67,11 @@ impl TryFrom<Marker> for Container {
     fn try_from(marker: Marker) -> Result<Self> {
         match marker {
             Marker::FourCC(b) => match &b {
-                b"FORM" => Ok(Container::IFF),
-                b"RIFF" => Ok(Container::RIFF),
-                b"RIFX" | b"FFIR" | b"XFIR" => Ok(Container::RIFX),
-                b"RF64" | b"BW64" => Ok(Container::RF64),
-                b"riff" => Ok(Container::SW64),
+                b"FORM" => Ok(Container::Interchange),
+                b"RIFF" => Ok(Container::ResourceInterchange),
+                b"RIFX" | b"FFIR" | b"XFIR" => Ok(Container::ResourceInterchangeBE),
+                b"RF64" | b"BW64" => Ok(Container::ResourceInterchange64),
+                b"riff" => Ok(Container::SonyWave64),
 
                 _ => Err(Error::UnknownContainer),
             },
@@ -75,11 +85,16 @@ impl TryFrom<Marker> for Container {
 impl fmt::Display for Container {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Container::IFF => write!(f, "IFF"),
-            Container::RIFF => write!(f, "RIFF"),
-            Container::RIFX => write!(f, "RIFX"),
-            Container::RF64 => write!(f, "RF64"),
-            Container::SW64 => write!(f, "SW64"),
+            Container::Interchange => write!(f, "Interchange File Format"),
+            Container::ResourceInterchange => write!(f, "Resource Interchange File Format"),
+            Container::ResourceInterchangeBE => {
+                write!(f, "Resource Interchange File Format (big-endian)")
+            }
+            Container::ResourceInterchange64 => {
+                write!(f, "Resource Interchange File Format (64-bit)")
+            }
+            Container::SonyWave64 => write!(f, "Sony Wave64"),
+            Container::CoreAudio => write!(f, "Core Audio Format"),
         }
     }
 }
