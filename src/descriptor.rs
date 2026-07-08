2aae0c32710bdcf31c6c84af47a95b86fa8b475a
ubmu <uubmu@proton.me>
2026-07-08 00:24:44 -0400

    Add clear modules and naming to ensure descriptors do not cause confusion.

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -21,6 +21,42 @@ pub struct Descriptor {
     pub size_width: u8,
 }
 
+/// The byte-boundary to align blocks to. For example, RIFF files must align to even byte-boundaries.
+/// For the case of a block payload ending at offset 139, a padding byte is needed to align to said boundary.
+mod align {
+    /// No padding required.
+    pub const NONE: u8 = 1;
+    /// Pad to 2-byte boundaries.
+    pub const EVEN: u8 = 2;
+    /// Pad to 8-byte boundary.
+    pub const EIGHT: u8 = 8;
+}
+
+/// The size of the marker field.
+mod marker {
+    pub const FOURCC: u8 = 4;
+    pub const UUID: u8 = 16;
+}
+
+/// The amount of bytes needed to read the size field.
+mod size {
+    pub const U32: u8 = 4;
+    pub const U64: u8 = 8;
+}
+
+/// The amount of bytes included in the declared size that is unrelated to the payload.
+/// Several formats store the block size rather than the payload size.
+mod overhead {
+    /// Zero overhead: the size field accurately describes the size of the block payload.
+    pub const NONE: u8 = 0;
+    /// 8-byte overhead: the size field contains 8 bytes of overhead.
+    /// For ISOBMFF, this is due to the 4-byte type and 4-byte size field being included in the declared size.
+    pub const EIGHT: u8 = 8;
+    /// 24-byte overhead: the size field contains 24 bytes of overhead.
+    /// For Sony Wave64, this is due to the 16-byte UUID and 8-byte size field being included in the declared size.
+    pub const TWENTY_FOUR: u8 = 24;
+}
+
 impl Descriptor {
     pub const fn new(
         byteorder: Byteorder,
@@ -39,12 +75,39 @@ impl Descriptor {
         }
     }
 
-    /// Descriptor for IFF and big-endian RIFF (RIFX) containers.
-    pub const IFF: Self = Self::new(Byteorder::Big, 2, 0, 4, 4);
-    /// Descriptor for RIFF, RF64, and BW64 containers.
-    pub const RIFF: Self = Self::new(Byteorder::Little, 2, 0, 4, 4);
-    /// Descriptor for Sony Wave64 containers.
-    pub const SW64: Self = Self::new(Byteorder::Little, 8, 24, 16, 8);
+    /// Descriptor for IFF and big-endian RIFF variants.
+    pub const INTERCHANGE: Self = Self::new(
+        Byteorder::Big,
+        align::EVEN,
+        overhead::NONE,
+        marker::FOURCC,
+        size::U32,
+    );
+    /// Descriptor for RIFF, RF64, and BW64.
+    pub const RESOURCE_INTERCHANGE: Self = Self::new(
+        Byteorder::Little,
+        align::EVEN,
+        overhead::NONE,
+        marker::FOURCC,
+        size::U32,
+    );
+    /// Descriptor for Sony Wave64.
+    pub const SONY_WAVE64: Self = Self::new(
+        Byteorder::Little,
+        align::EIGHT,
+        overhead::TWENTY_FOUR,
+        marker::UUID,
+        size::U64,
+    );
+
+    // Descriptor for Core Audio Format.
+    pub const CORE_AUDIO: Self = Self::new(
+        Byteorder::Big,
+        align::NONE,
+        overhead::NONE,
+        marker::FOURCC,
+        size::U32,
+    );
 
     /// Returns the number of padding bytes required after a payload of the
     /// given size to reach the next alignment boundary.
